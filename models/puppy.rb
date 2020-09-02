ffcc11a12f43c65b0bfcdbf724e689ef180d8e41
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-09-02 05:14:27 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,7 +2,7 @@ class Puppy
   
   attr_accessor :name, :breed, :age 
   
-  def initialize(name, breed, age)
+  def initialize(name:, breed:, age:)
     @age = age
     @name = name 
     @breed = breed
